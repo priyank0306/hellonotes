f8719be5ddacc8f2488b53db01c0cab578782495
priyank0306 <priyankmehta6777@gmail.com>
2022-12-07 22:29:45 +0530

    step-7

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,8 @@
+import 'package:hellonotes/services/auth/auth_user.dart';
+abstract class AuthProvider{
+  AuthUser?get currentUser;
+  Future<AuthUser?>logIn({
+    required String email,
+  required String password,
+  });
+}
